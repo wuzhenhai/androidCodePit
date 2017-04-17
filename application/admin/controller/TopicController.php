3b96f7daa41c34480f1a16f1ee55b5946c50e10b
wuzhenhai <1986wzh>
2017-04-17 16:28:17 +0800

    小更新

--- application/admin/controller/TopicController.php
+++ application/admin/controller/TopicController.php
@@ -1,6 +1,7 @@
 <?php
 namespace application\admin\controller;
 use application\admin\model\Topic;
+use think\Db;
 
 /**
  * author   :  mzj
@@ -11,40 +12,102 @@ class TopicController extends CommonController {
 
     public function index()
     {
-        $res=new Topic();
-        $lists=$res->getList();
+        $where = "";
+        $page_count = 10;//每页显示的数量
+        $skill_id = 0;
+
+        //设置技能类别
+        if(isset($_REQUEST['skill_type'])){
+            $skill_id = $_REQUEST['skill_type'];
+            if($skill_id!=0) {
+                $where = "skill_id='" . $skill_id . "'";
+            }
+            $this->assign("skill_type",$skill_id);
+        }
+
+        $topic_obj=new Topic();
+
+        $lists=$topic_obj->getList($where,$page_count,$skill_id);
+        $page = $lists->render();//分页
+        //获取技能列表
+        $select_skill_list = $topic_obj->getSkillList();
+        //var_dump($select_topic_list);
         $this->assign("lists",$lists);
+        $this->assign("page",$page);
+        $this->assign("skill_list",$select_skill_list);
         return $this->fetch();
     }
 
-
-//    public function add_topic_info(){
-//        $tech_list=db('tech_group') ->select();
-//        $this->assign("tech_list", $tech_list);
-//
-//        $module_list=new ModuleGroup();
-//        $module_list=$module_list->getGroups();
-//        $this->assign("module_list", $module_list);
-//
-//        return $this->fetch();
-//    }
-//
-//
-//    public function add(){
-//        $data = input();
-//        $data['module_id'] = intval($data['module_id']);
-//
-//        $module_name= Db::name('module_group')->where('module_id','=',  $data['module_id'] )->find();
-//        $data{'module_name'}=$module_name['module_name'];
-//
-//        $topic=new TopicGroup();
-//        $topic->addTopic($data);
-//
-//        if ($topic){
-//            $this->success('操作成功', url('index'));
+//    public function ajax_get_topic(){
+//        $topic_id = isset($_POST['topic_id'])?$_POST['topic_id']:0;
+//        $topic_obj = new Topic();
+//        $topic = $topic_obj->getTopicById($topic_id);
+//        if($topic){
+//            $resp['success'] = true;
+//            $resp['code'] = 0;
+//            $resp['data'] = json_encode($topic);
+//            exit(json_encode($resp));
 //        }else{
-//            $this->error('操作失败');
+//            $resp['success'] = false;
+//            $resp['code'] = 1;
+//            $resp['msg'] = "添加失败";
+//            exit(json_encode($resp));
 //        }
 //    }
 
+    /**
+     * 添加
+     */
+    public function add(){
+        $data = input();
+        //exit(print_r($data));
+        $topic_obj = new Topic();
+        if(!empty($data['topic_name'])){
+            $r = $topic_obj-> allowField(true)->save($data);
+            $resp = array();
+            if($r){
+                //稍微规范点的返回
+                $resp['success'] = true;
+                $resp['code'] = 0;
+                exit(json_encode($resp));
+            }else{
+                $resp['success'] = false;
+                $resp['code'] = 1;
+                $resp['msg'] = "添加失败";
+                exit(json_encode($resp));
+            }
+        }
+    }
+
+    /**
+     * 编辑
+     */
+    function edit(){
+        $data = input();
+        $topic_obj = new Topic();
+        $r = $topic_obj->edit($data['id'],$data);
+        if($r){
+            exit("success");
+        }else {
+            exit("failure");
+        }
+    }
+
+    /**
+     * 删除
+     */
+    public function del()
+    {
+        $data = input();
+        if (empty($data['id'])){
+            //$this->error('id缺失');
+            exit("id缺失");
+        }
+        $res = Db::name('topic')->where($data)->delete();
+        if ($res) {
+            exit("success");
+        } else {
+            exit("failure");
+        }
+    }
 }
--- application/admin/model/Topic.php
+++ application/admin/model/Topic.php
@@ -8,9 +8,31 @@ namespace application\admin\model;
 class  Topic extends \think\Model {
 
 
-    function getList($where=''){
-      return  $this ->where($where)->paginate(10);
+    function getList($where='',$page_count,$skill_type){
+        if($skill_type!=0) {
+            return $this->where($where)->paginate($page_count, false, ['query' => ['skill_type' => $skill_type]]);
+        }else{
+            return $this->where($where)->paginate($page_count);
+        }
     }
 
+    function getSkillList($where=""){
+        $obj_skill = new Skill();
+        return $obj_skill->where($where)->select();
+    }
+
+    function getTopicById($id){
+        return $this->where("id='".$id."'")->find();
+    }
+
+    function edit($id,$data=array()){
+        $res =  $this->where("id='".$id."'")->find();
+        if($res){
+            //后面的["tech_id" => $id]表示，更新这个id的数据
+            return $this->save($data,["id" => $id]);
+        }else{
+            return false;
+        }
+    }
 
 }
