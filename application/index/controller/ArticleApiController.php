eaf33e6504ffbda704f84997cd45477d002f2206
振坚 <598750822@qq.com>
2017-05-22 16:26:18 +0800

    update

--- application/index/controller/ArticleApiController.php
+++ application/index/controller/ArticleApiController.php
@@ -14,10 +14,60 @@ use application\index\model\Article;
 class ArticleApiController extends BaseApiController
 {
 
+    /**
+     * 根据条件获取文章列表(LvDom.article.getArticleList)
+     * @author mzj
+     * @param array
+     * @return 成功返回$article_list，失败返回错误码
+     */
     public function getArticleList($param) {
 
         $is_hot = intval($param['is_hot']); // 是否只获取热门文章
-        $where = $is_hot ? 'is_hot = 1' : '';//判断是否
+        $first_row = intval($param['first_row']); // 开始行，默认0
+        $fetch_num = intval($param['fetch_num']); // 获取行数，默认10
+
+        $first_row = $first_row ? $first_row : 0;
+        $fetch_num = $fetch_num ? $fetch_num : 10;
+
+        $where = $is_hot ? 'is_hot = 1' : '';//判断是否为推荐
+
+        //根据条件获取列表
+        $article_obj = new Article();
+        $article_list = $article_obj->getArticleList($where,$first_row,$fetch_num);
+
+        //总数
+        $total = $article_obj->getArticleNum($where);
+
+        $article_arr = array(
+            'total_num'    => $total ? $total : 0,  //总数
+            'nextFirstRow' => $first_row + $fetch_num,  //下一条开始位置
+            'article_list' => $article_list,
+        );
+
+        return $article_arr;
+    }
+
+    /**
+     * 获取热门文章列表(LvDom.article.getHotArticleList)
+     * @author mzj
+     * @param array
+     * @return 成功返回$article_list，失败返回错误码
+     */
+    public function getHotArticleList($param) {
+
+        $is_hot = intval($param['is_hot']); // 是否只获取热门文章
+        $first_row = intval($param['first_row']); // 开始行，默认0
+        $fetch_num = intval($param['fetch_num']); // 获取行数，默认10
+
+        $first_row = $first_row ? $first_row : 0;
+        $fetch_num = $fetch_num ? $fetch_num : 10;
+
+        $is_hot ? 'is_hot = 1' : '';//判断是否为推荐
+//        var_dump($is_hot);
+//        var_dump($first_row);
+//        var_dump($fetch_num);
+
+        $where = $is_hot ? 'is_hot = 1' : '';//判断是否为推荐
 
         $limit=$is_hot ==1?3:'';
 
@@ -26,6 +76,9 @@ class ArticleApiController extends BaseApiController
         $article_list = $article_obj->getArticleList($where,$limit);
         return $article_list;
     }
+
+
+
 
     //构造验证参数
     function getParams($func_name)
@@ -43,7 +96,30 @@ class ArticleApiController extends BaseApiController
                     'type_code'	=> 45008,
                     'func'		=> '',
                     'func_code'	=> 47008,
+                ),
+                //各种参数
+                array(
+                    'field'		=> 'first_row',
+                    'type'		=> 'string',
+                    'required'	=> false,
+                    'miss_code'	=> 41008,
+                    'empty_code'=> 44008,
+                    'type_code'	=> 45008,
+                    'func'		=> '',
+                    'func_code'	=> 47008,
+                ),
+                //各种参数
+                array(
+                    'field'		=> 'fetch_num',
+                    'type'		=> 'string',
+                    'required'	=> false,
+                    'miss_code'	=> 41008,
+                    'empty_code'=> 44008,
+                    'type_code'	=> 45008,
+                    'func'		=> '',
+                    'func_code'	=> 47008,
                 )
+
 
             ),
         );
